4166d1b39b80c5be9a0df392bc382cef513830f2
Robel Tesfu <sys.robel@gmail.com>
2026-07-31 14:33:53 +0300

    Time: 40 ms (16.12%), Space: 26.9 MB (64.77%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+
+    int solve(int i, int j, string &s, string &t) {
+
+        if (j == t.size())
+            return 1;
+
+        if (i == s.size())
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (s[i] == t[j]) {
+            return dp[i][j] =
+                solve(i + 1, j + 1, s, t) +
+                solve(i + 1, j, s, t);
+        }
+
+        return dp[i][j] = solve(i + 1, j, s, t);
+    }
+
+    int numDistinct(string s, string t) {
+
+        dp.assign(s.size(), vector<int>(t.size(), -1));
+
+        return solve(0, 0, s, t);
+    }
+};
